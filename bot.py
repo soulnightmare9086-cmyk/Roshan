7260dbef8aa30f8adf2d411f2e1c17c98533be0f
soulnightmare9086-cmyk <soulnightmare9086@gmail.com>
2026-05-20 17:22:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,8 +57,8 @@ async def start(update: Update, context):
 
 async def attack(update: Update, context):
     user_id = update.effective_user.id
-    
-        if len(context.args) < 4:
+   
+    if len(context.args) < 4:
            await update.message.reply_text(
             "❌ *Usage:*\n"
             "`/attack <IP> <PORT> <DURATION> <METHOD>`\n\n"
